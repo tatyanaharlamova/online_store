410ab164cc0945e39fae6a446f4c4e5f1cc6fd50
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-05-18 22:29:57 +0300

    Add docstrings

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -5,6 +5,9 @@ register = template.Library()
 
 @register.filter()
 def media_filter(path):
+    """
+    Шаблонный фильтр, который преобразует переданный путь в полный путь для доступа к медиафайлу.
+    """
     if path:
         return f"/media/{path}"
     return "#"
