cc6426fbe6adffa160f2d38fb64d312b351cba34
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-06-06 21:04:45 +0300

    Add forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ContactCreateView
+from catalog.views import (ProductListView, ProductDetailView, ProductCreateView, ProductDeleteView, ProductUpdateView,
+                           ContactCreateView)
 
 app_name = CatalogConfig.name
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactCreateView.as_view(), name='contacts'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='products_detail'),
+    path('create_product/', ProductCreateView.as_view(), name='create_product'),
+    path('delete_product/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
+    path('edit_product/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from catalog.models import Product, Contact
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Contact, Version
 
 
 class ProductListView(ListView):
@@ -10,12 +12,76 @@ class ProductListView(ListView):
     """
     model = Product
 
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        list_product = Product.objects.all()
+
+        for product in list_product:
+            version = Version.objects.filter(product=product)
+            activ_version = version.filter(is_active=True)
+            if activ_version:
+                product.active_version = activ_version.last().name
+                product.number_version = activ_version.last().number
+            else:
+                product.active_version = 'Нет активной версии'
+
+        context_data['object_list'] = list_product
+        return context_data
+
 
 class ProductDetailView(DetailView):
     """
     Контроллер, который отвечает за отображение информации о конкретном продукте
     """
     model = Product
+
+
+class ProductCreateView(CreateView):
+    """
+    Контроллер, который отвечает за создание продукта
+    """
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductUpdateView(UpdateView):
+    """
+    Контроллер, который отвечает за редактирование продукта
+    """
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('catalog:products_detail', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
+        if self.request.method == "POST":
+            context_data["formset"] = ProductFormset(self.request.POST, instance=self.object)
+        else:
+            context_data["formset"] = ProductFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data["formset"]
+        if form.is_valid and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+
+class ProductDeleteView(DeleteView):
+    """
+    Контроллер, который отвечает за удаление продукта
+    """
+    model = Product
+    success_url = reverse_lazy('catalog:products_list')
 
 
 class ContactCreateView(CreateView):
