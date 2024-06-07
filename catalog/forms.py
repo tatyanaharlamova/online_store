09f637ef3bcd62585e7770de4cd0d41ca0b68853
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-06-07 18:45:01 +0300

    Fix changes

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,16 +1,16 @@
 from django import forms
-from django.forms import BooleanField
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from catalog.models import Product, Version
 
 
 class StyleFormMixin:
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            if isinstance(field, BooleanField):
-                field.widget.attrs['class'] = 'form-check-input'
-            else:
+            if not isinstance(field.widget, forms.CheckboxInput):
                 field.widget.attrs['class'] = 'form-control'
 
 
@@ -44,3 +44,15 @@ class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = '__all__'
+
+
+class VersionFormSet(BaseInlineFormSet):
+
+    def clean(self):
+        active_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_activе'):
+                active_count += 1
+        if active_count > 1:
+            raise ValidationError("Может существовать только одна активная версия")
+        super().clean()
