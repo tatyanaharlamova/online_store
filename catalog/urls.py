98bdbb2d8edc159b51d161460ab9b4086d2f74af
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-06-25 11:52:14 +0300

    Add caching

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import (ProductListView, ProductDetailView, ProductCreateView, ProductDeleteView, ProductUpdateView,
-                           ContactCreateView)
+                           ContactCreateView, CategoryListView)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactCreateView.as_view(), name='contacts'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='products_detail'),
+    path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='products_detail'),
     path('create_product/', ProductCreateView.as_view(), name='create_product'),
     path('delete_product/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
     path('edit_product/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
+    path('categories/', CategoryListView.as_view(), name='categories_list')
 ]
--- config/settings.py
+++ config/settings.py
@@ -154,3 +154,13 @@ EMAIL_HOST_PASSWORD = MY_EMAIL_HOST_PASSWORD
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
+
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379'
+        }
+    }
